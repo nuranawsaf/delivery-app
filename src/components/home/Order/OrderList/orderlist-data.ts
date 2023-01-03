d209b24303a3e05511c159faada92626b815b715
nuranawsaf <nuranawsaf712@gmail.com>
2023-01-03 23:50:48 +0600

    add code

--- src/components/home/Order/OrderList/orderlist-data.ts
+++ src/components/home/Order/OrderList/orderlist-data.ts
@@ -68,4 +68,22 @@ export const orderlistdata: Array<OrderListData> = [
     btnIcon: IoMdBicycle,
     btn: 'Details',
   },
+  {
+    title: 'Order #315',
+    date: '5 Dec 2022,13:27',
+    loadIcon: CgTimelapse,
+    loadTime: '18:30',
+    loadTime2: '/30:00',
+    btnIcon: IoMdBicycle,
+    btn: 'Ready for collection',
+  },
+  {
+    title: 'Order #316',
+    date: '5 Dec 2022,13:29',
+    loadIcon: CgTimelapse,
+    loadTime: '01:30',
+    loadTime2: '/02:30',
+    btnIcon: CgTimelapse,
+    btn: 'Accept',
+  },
 ];
